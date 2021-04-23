6cb83d008a9efb56aefe38cbec9f2b3de13667b5
Murrayvmalt <murrayvmalt@gmail.com>
2021-04-23 16:40:43 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,28 @@
+var todos = [
+	"clean room",
+	"brush teeth",
+	"exercise",
+	"study",
+	"eat healthy"
+];
+
+for (var i=0; i < todos.length; i++) {
+	console.log(i);
+}
+
+todos.forEah(function(todo, i) {
+	console.log(todo, i);
+})
+
+// var CounterOne = 0;
+// while (CounterOne < 10) {
+// 	console.log(CounterOne);
+// 	CounterOne++
+// }
+
+// var counterTwo = 10
+// do {
+// 	console.log(counterTwo)
+// 	counterTwo--
+// } while (counterTwo > 0);
+
